62a6fb0a221c7079093894efccab82fba6dffc58
Ogunsola oluwatimileyn <oyelekeogunsola@gmail.com>
2020-01-13 22:58:35 +0100

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by user on 13/01/2020.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
